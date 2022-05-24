f492fb8e6139e5aaba4987e29309173c5dbe60ea
Zeshan Munir <zmunir93@gmail.com>
2022-05-24 14:08:40 -0400

    pulling data from weather api site

--- public/script.js
+++ public/script.js
@@ -1,24 +1,41 @@
 let autocomplete;
+let userLocation;
 
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
         document.getElementById('autocomplete'),
         {
-            types: ['postal_code', 'locality'],
+            types: ['locality'],
             fields: ['place_id', 'geometry', 'name', 'formatted_address', 'address_component']
         });
 
     autocomplete.addListener('place_changed', onPlaceChanged);
 }
 
-function onPlaceChanged() {
-    var place = autocomplete.getPlace();
+function onPlaceChanged() {   
 
-    console.log(place);
+    var place = autocomplete.getPlace();
+    userLocation = place.name;
+    console.log(userLocation);
+    console.log(typeof userLocation);
+
+    
+    let params = new URLSearchParams({
+        access_key: '33b75764c314778e974be58e0d3eb310',
+        query: userLocation,
+        units: 'f'
+    })
+    
+    fetch(`http://api.weatherstack.com/current?${params}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
 
     if (!place.geometry) {
         document.getElementById('autocomplete').placeholder = 'Enter a place';
     } else {
+        document.getElementById('details').innerHTML = place.name;
         document.getElementById('details').innerHTML = place.name;
     }
 }
